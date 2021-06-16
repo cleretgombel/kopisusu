c5603824a213fd435d1a4c9724578bb0b91c05d5
cleretgombel <76208420+cleretgombel@users.noreply.github.com>
2021-06-16 23:01:49 +0700

    Create ruwetenan.php

---
+++ ruwetenan.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<br /><html><head></head><body><teamtalk version="5.4">
+    <host>
+        <name>Selamat Datang Di Halaman Login Dan Info Dari New O.F.A..Untuk Acara Doa Bersama Rutin Di Adakan Setiap Malam Jumat Di mulai Setiap Jam 20:00 Wib Atau Jam 8 malam 21:00 Waktu Hongkong/Taiwan/Malaysia.. Terimaksih Telah Mengunakan Selalu Dan Bergabung Di New O.F.A..</name>
+        <address>ofa.indonesia</address>
+        <tcpport>1003</tcpport>
+        <udpport>1003</udpport>
+        <encrypted>false</encrypted>
+        <auth>
+            <username>X</username>
+            <password>3</password>
+        </auth>
+        <join>
+            <channel></channel>
+            <password></password>
+        </join>
+        <clientsetup>
+            <voice-activated>-1</voice-activated>
+        </clientsetup>
+    </host>
+</teamtalk></body></html>
